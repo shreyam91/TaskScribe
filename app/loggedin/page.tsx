86a2784fb1e74a8408c30cdca600cf8d80abac14
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-09 23:49:15 +0530

    user profile & navigation done...

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,15 +1,15 @@
-// import MobileNav from "@/components/nav/mobile-nav";
-// import SideBar from "@/components/nav/side-bar";
+import MobileNav from "@/components/nav/mobile-nav";
+import SideBar from "@/components/nav/side-bar";
 // import TodoList from "@/components/todos/todo-list";
 
-import UserProfile from "@/components/todo/user-profile";
+import UserProfile from "@/components/nav/user-profile";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      {/* <SideBar /> */}
+      <SideBar />
       <div className="flex flex-col">
-        {/* <MobileNav /> */}
+        <MobileNav />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
           {/* <TodoList /> */}
 
